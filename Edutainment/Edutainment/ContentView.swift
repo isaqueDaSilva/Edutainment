204edcdfcf405506bb70c26cfb4bb7f41dd56c66
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-07-10 23:56:26 -0300

    Edutainment

--- Edutainment/Edutainment/ContentView.swift
+++ Edutainment/Edutainment/ContentView.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     @State private var showingSteps = false
-    @State private var steps = 1
+    @State private var step = 1
     
     @State private var startGame = false
+    
+    @State private var tableSelect1 = 1
+    @State private var tableSelect2 = 2
+    @State private var tableSelect3 = 3
+    
+    @ViewBuilder var stepButton: some View {
+        VStack {
+            Button("Next", action: {
+                step += 1
+            })
+            .buttonStyle(.borderedProminent)
+        }
+    }
+    
+    let tables = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     
     var body: some View {
         ZStack {
@@ -36,8 +50,24 @@ struct ContentView: View {
                     .buttonStyle(.borderedProminent)
                     
                     VStack {
+                        Text("Before starting the game:")
+                            .font(.title2.bold())
                         
+                        if step == 1 {
+                            Picker("Select which multiplication tables you want to practice?", selection: $tableSelect1) {
+                                ForEach(tables, id: \.self) {
+                                    Text("\($0)")
+                                }
+                            }
+                        }
+                        
+                        stepButton
                     }
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 20)
+                    .background(.thinMaterial)
+                    .cornerRadius(20)
+                    .shadow(radius: 10)
                 }
             }
         }
