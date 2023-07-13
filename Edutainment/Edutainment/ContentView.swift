8d2c61b03972ea7783d54bf1001aacf0bca1caa3
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-07-13 15:38:55 -0300

    Edutainment

--- Edutainment/Edutainment/ContentView.swift
+++ Edutainment/Edutainment/ContentView.swift
@@ -39,7 +39,7 @@ struct ContentView: View {
     
     @State private var difficultyLevelSelected: DifficultyLevel = .easy
     
-    let multiplier = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].shuffled()
+    let multiplier = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].shuffled()
     var multiplying: [Int] {
         var question = [Int]()
         
