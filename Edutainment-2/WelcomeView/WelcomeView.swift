153b20ef56474bd0ce70e079642203ae29f68df2
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-07-15 13:53:05 -0300

    WelcomeView of Edutainment

--- Edutainment-2/WelcomeView/WelcomeView.swift
+++ Edutainment-2/WelcomeView/WelcomeView.swift
@@ -8,9 +8,8 @@
 import SwiftUI
 
 struct WelcomeView: View {
-    @State private var welcomeView = true
     @State private var showingSteps = false
-    @State private var step = 1
+    @State private var step = 0
     @State private var difficultyLevel: DifficultyLevel = .easy
     @State private var numberOfQuestionsSelected = 5
     
@@ -18,73 +17,79 @@ struct WelcomeView: View {
     
     var body: some View {
         VStack {
-            if welcomeView == true {
-                Text("Welcome to Edutainment 🤓")
-                    .font(.title.bold())
-                if showingSteps == false {
-                    Text("The perfect space for fun and learning at the same time😉")
+            Text("Welcome to Edutainment")
+                .font(.title.bold())
+            
+            if showingSteps == false {
+                Text("The perfect space for fun and learning at the same time 😉")
+                    .font(.subheadline.bold())
+                    .multilineTextAlignment(.center)
+                
+                Button("Play", action: {
+                    withAnimation {
+                        showingSteps = true
+                        step = 1
+                    }
+                })
+                .buttonStyle(.borderedProminent)
+            }
+            
+            VStack{
+                Spacer()
+                
+                Text("Before Starting Game:")
+                    .font(.title2.bold())
+                
+                Spacer()
+                
+                if step == 1 {
+                    Text("What difficulty level would you like to tackle?")
                         .font(.headline.bold())
                         .multilineTextAlignment(.center)
                     
-                    Button("Play", action: {
-                        withAnimation {
-                            showingSteps = true
+                    Picker("Difficulty Level", selection: $difficultyLevel) {
+                        ForEach(DifficultyLevel.allCases, id: \.self) {
+                            Text($0.rawValue)
                         }
-                    })
-                    .buttonStyle(.borderedProminent)
+                    }
+                    .pickerStyle(.segmented)
+                    .padding()
                 }
                 
-                VStack {
-                    Spacer()
-                    Text("Before starting the game:")
-                        .font(.title3.bold())
-                    Spacer()
-                    if step == 1 {
-                        Text("What difficulty level would you like to tackle?")
-                            .font(.headline.bold())
-                            .multilineTextAlignment(.center)
-                        
-                        Picker("Difficulty Level", selection: $difficultyLevel) {
-                            ForEach(DifficultyLevel.allCases, id: \.self) {
-                                Text($0.rawValue)
-                            }
-                        }
-                        .pickerStyle(.segmented)
-                        .padding()
-                    }
+                if step == 2 {
+                    Text("How many questions would you like to answer?")
+                        .font(.headline.bold())
+                        .multilineTextAlignment(.center)
                     
-                    if step == 2 {
-                        Text("How many questions would you like to answer?")
-                            .font(.headline.bold())
-                            .multilineTextAlignment(.center)
-                        
-                        Picker("Number of Question", selection: $numberOfQuestionsSelected) {
-                            ForEach(numberOfQuestions, id: \.self) {
-                                Text(String($0))
-                            }
+                    Picker("Number of Questions", selection: $numberOfQuestionsSelected) {
+                        ForEach(numberOfQuestions, id: \.self) {
+                            Text(String($0))
                         }
-                        .pickerStyle(.segmented)
-                        .padding()
                     }
-                    Spacer()
-                    Button(step == 1 ? "Next" : "Start Game", action: {
-                        if step == 1 {
-                            step += 1
-                        } else {
-                            showingSteps = false
-                            welcomeView = false
-                        }
-                    })
-                    .buttonStyle(.borderedProminent)
-                    Spacer()
+                    .pickerStyle(.segmented)
+                    .padding()
                 }
-                .frame(maxWidth: 350, maxHeight: showingSteps ? 250 : 0)
-                .background(.thinMaterial)
-                .cornerRadius(20)
-                .shadow(radius: 10)
-                .padding()
+                
+                Spacer()
+                
+                Button(step == 1 ? "Next" : "Start Game", action: {
+                    if step == 1 {
+                        step += 1
+                    } else {
+                        showingSteps = false
+                    }
+                })
+                .buttonStyle(.borderedProminent)
+                
+                Spacer()
             }
+            .frame(maxWidth: 350, maxHeight: showingSteps ? 250 : 0)
+            .background(.thinMaterial)
+            .cornerRadius(20)
+            .shadow(radius: 10)
+            .padding()
         }
+        .padding()
     }
 }
 
